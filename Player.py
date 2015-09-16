9339fcaa7aab321b75374a31d65cedfb72e1ef23
Casual Projects <jakebellock@gmail.com>
2015-09-15 23:07:35 -0500

    Working version up to getting odds and writing to a csv file

---
+++ Player.py
@@ -0,0 +1,12 @@
+
+
+class Player:
+
+    def __init__(self):
+        self.Name = ''
+        self.Salary = 0
+        self.Team = ''
+        self.GameInfo = ''
+        self.Position = ''
+        self.AvgPoints = 0
+
